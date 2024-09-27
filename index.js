bcd56287c04e15e098953d3afee1887d701c0c08
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2024-09-27 14:50:59 +0330

    complete login html css

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+let LOGIN_FLAG = false;
+
+const signupContainer = document.querySelector('.container');
+const loginContainer = document.querySelector('.login-container');
+
+const changeMode = () => {
+    // if (LOGIN_FLAG) {
+    //     signupContainer.style.display = 'none';
+    //     loginContainer.style.display = 'flex';
+    // } else {
+    //     signupContainer.style.display = 'flex';
+    //     loginContainer.style.display = 'none';
+    // }
+    signupContainer.classList.toggle('active');
+    loginContainer.classList.toggle('active');
+}
+
+changeMode();
+
+const changeBtn = document.querySelectorAll('.change a');
+
+changeBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        LOGIN_FLAG = !LOGIN_FLAG;
+        changeMode();
+    });
+});
+
 const visibilityPasswordBtn = document.querySelectorAll('.right input + span');
 
 const visibilityPasswordCallback = () => {
